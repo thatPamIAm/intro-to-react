dbedae629844287da32fddbbbd7057dcbbc305e3
Leta Keane <letakeane+turing@gmail.com>
2018-04-09 11:38:14 -0600

    Add method to increment count

--- src/EvilDeedCounter.js
+++ src/EvilDeedCounter.js
@@ -4,16 +4,22 @@ export default class EvilDeedCounter extends Component {
   constructor() {
     super();
     this.state = {
-
+      count: 0
     }
+    // this.updateCount = this.updateCount.bind(this);
+  }
+
+  updateCount() {
+    let newCount = this.state.count + 1;
+    this.setState({ count: newCount });
   }
 
   render() {
     return (
       <div>
         <h3>Evil Deeds Committed Today</h3>
-        <h2>0</h2>
-        <button>+1</button>
+        <h2>{this.state.count}</h2>
+        <button onClick={() => this.updateCount()}>+1</button>
       </div>
     )
   }
